b5f0b9b7fc23ec2fbe3d344f1acb8dfa14a538b9
darkcat974 <florian.dajon@epitech.eu>
2024-01-31 12:21:40 +0400

    j'aime pas

--- 5.My_Getnbr/My_Getnbr.asm
+++ 5.My_Getnbr/My_Getnbr.asm
@@ -2,4 +2,26 @@ section .text
     global my_Getnbr
 
 my_Getnbr:
-    
+    init_val:
+        xor r8, r8
+        xor rax, rax
+        xor rcx, rcx
+
+    get_loop:
+        movzx r8, byte[rdi + rcx]
+
+        cmp r8, 0
+        je end_get
+        cmp r8, '0'
+        jl end_get
+        cmp r8, '9'
+        jg end_get
+        sub r8, '0'
+        imul rax, rax, 10
+        add rax, r8
+        inc rcx
+        jmp get_loop
+
+    end_get:
+        ret
+
